04482c02ccbc790ee8cf72928767b3241e6ff1bb
yhunlu <yhunlu87@gmail.com>
2022-02-14 23:21:47 +0300

    initial setup for nextjs-tailwindcss-jsx developers

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home = () => {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
+
+export default Home;
